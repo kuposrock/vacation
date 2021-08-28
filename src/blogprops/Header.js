ef0594ddc824ba18277827e93991c7c53695fcbe
Matt <kuposrock@gmail.com>
2021-08-27 17:30:57 -0700

    Small updates to components

--- src/blogprops/Header.js
+++ src/blogprops/Header.js
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <div className="header">
              <Navbar className="navbar"/>
-             <h1>Clean Blog</h1>
+             <h1 className="header">Clean Blog</h1>
              <p>A Custom Blog Theme by React</p>
         </div>
     )
