c5d9dccaad6b8859296725aacdd192bdf6bebb16
Matt <kuposrock@gmail.com>
2021-09-06 17:53:10 -0700

    Finished Dice Component

--- src/ReactDice/Dice.js
+++ src/ReactDice/Dice.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
 export default function Dice(props) {
+    var selected = false;
+    function selectDice(){
+    if(!selected){
+        selected=true;
+        console.log(selected);
+    }
+    else{
+        selected=false;
+        console.log(selected);
+    }
+    }
     return (
-        <div className="dice"> 
+        <div className="dice" onClick={()=>{selectDice()}}> 
 
            <p> {props.number}</p>
         </div>
