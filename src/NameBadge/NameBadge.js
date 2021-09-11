7fc58a241225e8c2b64d7b6bd64dddbb080f9e03
Matt <kuposrock@gmail.com>
2021-09-10 18:08:38 -0700

    Need to add input restrictions still

--- src/NameBadge/NameBadge.js
+++ src/NameBadge/NameBadge.js
@@ -4,39 +4,66 @@ import React from 'react';
 class NameEntry extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { fName:"",lName:"",email:"",pob:"",phone:"",food:"",description:""};
-        
+        this.state = { fName: "", lName: "", email: "", pob: "", phone: "", food: "", description: "", items: [] };
+
     }
     mySubmitHandler = (event) => {
         event.preventDefault();
-        this.setState({ names: this.state.names.concat(this.state.username)});
-        
+        var values = this.state.items.concat({ fName: this.state.fName, lName: this.state.lName, email: this.state.email, pob: this.state.pob, phone: this.state.phone, food: this.state.food, description: this.state.description });
+        this.setState({ items: values });
+        console.log(this.state);
     }
     myChangeHandler = (event) => {
-        this.setState({ username: event.target.value });
+        console.log(event.target.value + event.target.name)
+        var name = event.target.name;
+        this.setState({ [name]: event.target.value });
     }
     render() {
         return (
-            <div className="container">
-            <form onSubmit={this.mySubmitHandler}>
-                <h1>Hello {this.state.fName}</h1>
-                <p>Enter your name, and submit:</p>
-                <label>First Name</label>
-                <input type='text' onChange={this.myChangeHandler} name="fName" />
-                <label>Last Name</label>
-                <input type='text' onChange={this.myChangeHandler} name="lName" />
-                <input type='text' onChange={this.myChangeHandler} name="email" />
-                <input type='text' onChange={this.myChangeHandler} name="pob" />
-                <input type='text' onChange={this.myChangeHandler} name="phone" />
-                <input type='text' onChange={this.myChangeHandler} name="food" />
-                <input type='text' onChange={this.myChangeHandler} name="description" />
-                <input type='submit' />
-            </form>
-            <ul>
-                {/* {this.state.names.map((name,index)=>{
-                    return (<li key={name +index}>{name}</li>)
-                })} */}
-            </ul>
+            <div>
+                <div className="container">
+                    <form onSubmit={this.mySubmitHandler}>
+                        <input type='text' onChange={this.myChangeHandler} name="fName" placeholder="First Name" />
+                        <input type='text' onChange={this.myChangeHandler} name="lName" placeholder="Last Name" />
+                        <input type='text' onChange={this.myChangeHandler} name="email" placeholder="Favorite Food" />
+                        <input type='text' onChange={this.myChangeHandler} name="pob" placeholder="Place of Birth" />
+                        <input type='text' onChange={this.myChangeHandler} name="phone" placeholder="Phone Number" />
+                        <input type='text' onChange={this.myChangeHandler} name="food" placeholder="Favorite Food" />
+                        <textarea type='text' onChange={this.myChangeHandler} name="description" placeholder="Description" className="description" />
+                        <input type='submit' />
+                    </form>
+                </div>
+
+                {
+                    this.state.items.map((item, index) => {
+                        return (
+                            <div key={item + index} className="container">
+                                <div className="col">
+                                    <p>First Name: {item.fName}</p>
+                                    <p>Place of Birth: {item.pob}</p>
+                                    <p>Email: {item.email}</p>
+                                </div>
+                                <div className="col">
+                                    <p>Last Name: {item.lName}</p>
+                                    <p>Phone Number: {item.phone}</p>
+                                    <p>Favorite Food: {item.food}</p>
+                                </div>
+                                
+                                <div className="desc">Description: {item.description}</div>
+                                
+
+                                {/* <p key={item.fName+index}>First Name: {item.fName}</p>
+                        <p key={item.lName+index}>Last Name: {item.lName}</p>
+                        <p key={item.email+index}>Email: {item.email}</p>
+                        <p key={item.pob+index}>Place of Birth: {item.pob}</p>
+                        <p key={item.phone+index}>Phone Number: {item.phone}</p>
+                        <p key={item.food+index}>Favorite Food: {item.food}</p>
+                        <p key={item.description +index}>Description: {item.description}</p> */}
+                            </div>
+                        )
+                    })
+                }
+
             </div>
         );
     }
