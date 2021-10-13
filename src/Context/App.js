4a386180739c48e726282acd7a8dec53b73f8f5c
Matt <kuposrock@gmail.com>
2021-10-13 13:43:40 -0700

    Context Practice

--- src/Context/App.js
+++ src/Context/App.js
@@ -1,13 +1,17 @@
 import React from "react"
-
+import './App.css';
 import Header from "./Header"
 import Button from "./Button"
+import ThemeContext from "./ThemeContext";
 
 function App() {
+
     return (
         <div>
+          <ThemeContext.Provider value={"light"}>
             <Header />
             <Button />
+          </ThemeContext.Provider>
         </div>
     )
 }
